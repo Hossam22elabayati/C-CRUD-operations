3745c71752fcd3c81ff9d71dba6a9da80c398f65
Hossam22elabayati <hossamelabayati@gmail.com>
2025-05-04 23:29:14 +0300

    only one intial commit

---
+++ products system/products system/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace products_system
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Trim(' ') =="osama"  && textBox2.Text=="123")
+            {
+                Form3 f3 = new Form3();
+                f3.Show();
+                this.Hide();
+                
+            }
+            else if (textBox1.Text.Trim(' ')=="" || textBox2.Text=="")
+            {
+                MessageBox.Show("enter password or user name ", "not sucess ", MessageBoxButtons.OK);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+    }
+}
---
+++ products system/products system/Form3.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace products_system
+{
+    
+    public partial class Form3 : Form
+    {
+        DataTable table = new DataTable("table");
+        int index;
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            table.Columns.Add("ID", Type.GetType("System.Int32"));
+            table.Columns.Add("Proudct_Name", Type.GetType("System.String"));
+            table.Columns.Add("Price", Type.GetType("System.Double"));
+            dataGridView1.DataSource = table;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            table.Rows.Add(textBox1.Text ,textBox5.Text,textBox4.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+            textBox5.Text = String.Empty;
+            textBox4.Text = String.Empty;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            index = e.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[index];
+            textBox1.Text = row.Cells[0].Value.ToString();
+            textBox5.Text = row.Cells[1].Value.ToString();
+            textBox4.Text = row.Cells[2].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow newdata = dataGridView1.Rows[index];
+            newdata.Cells[0].Value = textBox1.Text;
+            newdata.Cells[1].Value = textBox5.Text;
+            newdata.Cells[2].Value = textBox4.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            index = dataGridView1.CurrentCell.RowIndex;
+            dataGridView1.Rows.RemoveAt(index);
+        }
+    }
+}
